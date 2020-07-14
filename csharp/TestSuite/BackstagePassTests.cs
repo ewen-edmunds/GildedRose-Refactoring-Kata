2ff06779f352b6b87c5f5fa7e0467c6771115fd6
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-14 13:23:06 +0100

    BackStagePassTests now tests quality updater

--- csharp/TestSuite/BackstagePassTests.cs
+++ csharp/TestSuite/BackstagePassTests.cs
@@ -13,9 +13,8 @@ namespace csharp
             {
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 100, Quality = 10}
             };
-            GildedRose app = new GildedRose(Items);
-
-            app.UpdateQuality();
+            QualityUpdater qualityUpdater = QualityFactory.QualityManager(Items[0]);
+            qualityUpdater.UpdateItemQuality(Items[0]);
 
             Assert.AreEqual(11, Items[0].Quality);
         }
@@ -27,9 +26,8 @@ namespace csharp
             {
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10}
             };
-            GildedRose app = new GildedRose(Items);
-
-            app.UpdateQuality();
+            QualityUpdater qualityUpdater = QualityFactory.QualityManager(Items[0]);
+            qualityUpdater.UpdateItemQuality(Items[0]);
 
             Assert.AreEqual(12, Items[0].Quality);
         }
@@ -41,9 +39,8 @@ namespace csharp
             {
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10}
             };
-            GildedRose app = new GildedRose(Items);
-
-            app.UpdateQuality();
+            QualityUpdater qualityUpdater = QualityFactory.QualityManager(Items[0]);
+            qualityUpdater.UpdateItemQuality(Items[0]);
 
             Assert.AreEqual(13, Items[0].Quality);
         }
@@ -55,9 +52,8 @@ namespace csharp
             {
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10}
             };
-            GildedRose app = new GildedRose(Items);
-
-            app.UpdateQuality();
+            QualityUpdater qualityUpdater = QualityFactory.QualityManager(Items[0]);
+            qualityUpdater.UpdateItemQuality(Items[0]);
 
             Assert.AreEqual(0, Items[0].Quality);
         }
@@ -71,11 +67,11 @@ namespace csharp
                 {
                     new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 40}
                 };
-                GildedRose app = new GildedRose(Items);
+                QualityUpdater qualityUpdater = QualityFactory.QualityManager(Items[0]);
                 
                 for (int j = 0; j < numDaysToSimulate; j++)
                 {
-                    app.UpdateQuality();
+                    qualityUpdater.UpdateItemQuality(Items[0]);
                 }
                 
                 Assert.LessOrEqual(Items[0].Quality, 50);
